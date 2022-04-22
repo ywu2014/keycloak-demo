c387a1a01d251f041786f22c79834a8e5dffad09
ywu2014 <yejunwu123@gmail.com>
2022-04-22 10:21:23 +0800

    feat: Spring Boot Adapter示例

---
+++ keycloak-adapter-springboot/src/main/java/com/ywu/keycloak/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.ywu.keycloak.controller;
+
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ProductController {
+
+    @ResponseBody
+    @GetMapping(path = "/protected/principle")
+    public Object getPrinciple(Model model, HttpServletRequest request){
+        KeycloakSecurityContext keycloakContext = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
+
+        AccessToken token = keycloakContext.getToken();
+        System.out.println("realm:" + keycloakContext.getRealm() + ", token:" + token.getAuthorization());
+
+        return token;
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "/";
+    }
+}
